a2a1b32ac33b25b5621ac42030b7a48bf60792f0
os.chenxx <os.chenxx@ffcs.cn>
2022-07-23 00:41:50 +0800

    dist

---
+++ dist/assets/avatar.d43413cf.js
@@ -0,0 +1,1 @@
+var a="./assets/avatar.38cdfa53.png";export{a as _};
---
+++ dist/assets/userInfo.daeb16fc.js
@@ -0,0 +1,1 @@
+import{s as r}from"./index.b40002c3.js";var s={getUserList:e=>r({url:"user/getUserList",method:"get",params:e}),getRoleList:e=>r({url:"role/getRoleList",method:"get",params:e})};export{s as u};
