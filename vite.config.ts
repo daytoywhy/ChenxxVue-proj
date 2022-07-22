b350f65e02f496917dae8fef3b26cee43776654a
os.chenxx <os.chenxx@ffcs.cn>
2022-07-23 00:46:31 +0800

    dist

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,4 @@
 import { defineConfig, loadEnv } from 'vite'
-import compress from 'vite-plugin-compress'
 import vue from '@vitejs/plugin-vue'
 const path = require('path')
 // https://vitejs.dev/config/
@@ -26,7 +25,6 @@ export default defineConfig(({ command, mode }) => {
     base: './',
     plugins: [
       vue(),
-      compress(),
     ],
     css: {
       preprocessorOptions: {
